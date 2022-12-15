b0129cf839595fbcfec9d2b904d6e1c99507cbe2
WhalesBob <ndw8200@naver.com>
2022-12-15 16:16:36 +0900

    feat(AllMapRepository) : AllMapRepository 에 정보를 올리는 기능 추가

--- src/main/java/subway/domain/AllMapRepository.java
+++ src/main/java/subway/domain/AllMapRepository.java
@@ -6,5 +6,24 @@ import org.jgrapht.graph.WeightedMultigraph;
 public class AllMapRepository {
     private static final WeightedMultigraph<String, DefaultDirectedGraph> distanceGraph = new WeightedMultigraph<>(DefaultDirectedGraph.class);
     private static final WeightedMultigraph<String, DefaultDirectedGraph> timeGraph = new WeightedMultigraph<>(DefaultDirectedGraph.class);
-    
+
+    public static void addOnDistanceGraph(String leftStation, String rightStation, int distance){
+        if(!distanceGraph.containsVertex(leftStation)){
+            distanceGraph.addVertex(leftStation);
+        }
+        if(!distanceGraph.containsVertex(rightStation)){
+            distanceGraph.addVertex(rightStation);
+        }
+        distanceGraph.setEdgeWeight(distanceGraph.addEdge(leftStation,rightStation),distance);
+    }
+
+    public static void addOnTimeGraph(String leftStation, String rightStation, int time){
+        if(!timeGraph.containsVertex(leftStation)){
+            distanceGraph.addVertex(leftStation);
+        }
+        if(!timeGraph.containsVertex(rightStation)){
+            distanceGraph.addVertex(rightStation);
+        }
+        timeGraph.setEdgeWeight(timeGraph.addEdge(leftStation,rightStation),time);
+    }
 }
